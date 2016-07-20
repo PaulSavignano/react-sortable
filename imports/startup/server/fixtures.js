7d28d6998efc9774728df51db9f51df986eccf45
Paul Savignano <Paul.Savignano@gmail.com>
2016-07-20 13:44:17 -0700

    Added Sortable component and method.

---
+++ imports/startup/server/fixtures.js
@@ -0,0 +1,44 @@
+import { Meteor } from 'meteor/meteor';
+import { Roles } from 'meteor/alanning:roles';
+import { Accounts } from 'meteor/accounts-base';
+import { Documents } from '../../api/documents/documents.js'
+
+const documents = [
+  { title: 'Document #1', order: 0 },
+  { title: 'Document #2', order: 1 },
+  { title: 'Document #3', order: 2 },
+  { title: 'Document #4', order: 3 },
+  { title: 'Document #5', order: 4 },
+  { title: 'Document #6', order: 5 },
+  { title: 'Document #7', order: 6 },
+]
+
+documents.forEach((doc) => {
+  const docExists = Documents.findOne({ title: doc.title })
+
+  if (!docExists) {
+    Documents.insert(doc)
+  }
+})
+
+
+
+
+
+const users = [{
+  email: 'admin@admin.com',
+  password: 'password',
+  profile: {
+    name: { first: 'Carl', last: 'Winslow' },
+  },
+  roles: ['admin'],
+}];
+
+users.forEach(({ email, password, profile, roles }) => {
+  const userExists = Meteor.users.findOne({ 'emails.address': email });
+
+  if (!userExists) {
+    const userId = Accounts.createUser({ email, password, profile });
+    Roles.addUsersToRoles(userId, roles);
+  }
+});
